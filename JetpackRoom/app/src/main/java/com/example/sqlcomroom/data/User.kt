6ade15ce287bfb179709366e416ed0ffee02ecfc
Jenifer Galdino <jenigaldino21@gmail.com>
2022-04-01 16:56:07 -0300

    jetpack room

---
+++ JetpackRoom/app/src/main/java/com/example/sqlcomroom/data/User.kt
@@ -0,0 +1,15 @@
+package com.example.sqlcomroom.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var nome: String,
+    var sobrenome: String,
+    var idade: Int
+        )
+{
+}
---
+++ JetpackRoom/app/src/main/java/com/example/sqlcomroom/data/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.example.sqlcomroom.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDAO: UserDAO) {
+    val lerTodosOsDados: LiveData<List<User>> = userDAO.lerTodosOsDados()
+    fun addUser(user: User){
+        userDAO.addUser(user)
+    }
+}
